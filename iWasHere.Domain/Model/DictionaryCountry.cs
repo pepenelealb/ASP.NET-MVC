5219839ed9b3b3f8420ecc4c7bb1e01ca4ae547d
afrentescu <adelafrentescu@gmail.com>
2019-09-16 15:12:48 +0300

    view

--- iWasHere.Domain/Model/DictionaryCountry.cs
+++ iWasHere.Domain/Model/DictionaryCountry.cs
@@ -7,12 +7,12 @@ namespace iWasHere.Web.Domain
     {
         public DictionaryCountry()
         {
-            DictionaryCounty = new HashSet<DictionaryCounty>();
+            dictionaryCountry = new HashSet<DictionaryCountry>();
         }
 
         public int CountryId { get; set; }
         public string CountryName { get; set; }
 
-        public virtual ICollection<DictionaryCounty> DictionaryCounty { get; set; }
+        public virtual ICollection<DictionaryCountry> dictionaryCountry { get; set; }
     }
 }
